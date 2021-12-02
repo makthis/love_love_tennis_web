c43dea488c771f815d375a82dfaddf27840e7fa9
Mudassir Chapra <muddi900@gmail.com>
2021-12-02 17:01:39 +0500

    fixed uri for firebase functions

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {
 "favicon.png": "920e01e70326ff12804686006aa2b609",
 "icons/Icon-192.png": "bfcf07227f0b91503971d763566ca4b5",
 "icons/Icon-512.png": "62c8393b0791f8c98cf386d8c1988c07",
-"index.html": "adac2ef4f0bb0d2bcb7452ac129347ea",
-"/": "adac2ef4f0bb0d2bcb7452ac129347ea",
-"main.dart.js": "e8abd78655f4a9259b6ed604801334b1",
+"index.html": "572c9c1e975d8ef8fd333de99d115a24",
+"/": "572c9c1e975d8ef8fd333de99d115a24",
+"main.dart.js": "b9ad2678ed2172feafabbd53d7945b29",
 "manifest.json": "bc285b1ae467d3ace39a21b43ce385a2",
 "version.json": "b766e645505ca813c5758ab00b87e2ad"
 };
